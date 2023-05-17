0dd1041c0837452c3229fc2bba1e232fc314debb
Matt <5974590+mattheson@users.noreply.github.com>
2023-05-16 20:15:59 -0700

    refactoring

--- main.py
+++ main.py
@@ -1,135 +1,112 @@
-import sounddevice as sd
-import numpy as np
-from whispercpp import Whisper
-import ffmpeg
-import time
-import os
-from datetime import datetime
-from scipy.io import wavfile
-import openai
 import json
+import numpy as np
+import sounddevice as sd
+from whispercpp import Whisper
+import openai
+import os
 
-# List all input devices
-devices = sd.query_devices()
-print("Input Devices:")
-for i, device in enumerate(devices):
-    if device['max_input_channels'] > 0:
-        print(f"{i}: {device['name']}")
+def choose_device():
+    devices = sd.query_devices()
+    print("Input Devices:")
+    for i, device in enumerate(devices):
+        if device['max_input_channels'] > 0:
+            print(f"{i}: {device['name']}")
 
-# Ask user to select an input device
-while True:
-    try:
-        device_id = int(input("Please enter the ID of the device you want to use: "))
-        if devices[device_id]['max_input_channels'] > 0:
+    while True:
+        try:
+            device_id = int(input("Please enter the ID of the device you want to use: "))
+            if devices[device_id]['max_input_channels'] > 0:
+                return device_id
+            else:
+                print(f"Device ID {device_id} is not an input device. Please try again.")
+        except (IndexError, ValueError):
+            print("Invalid device ID. Please try again.")
+
+def create_stream(callback, device_id, sample_rate=16000):
+    sd.default.device = device_id
+    stream = sd.InputStream(callback=callback, channels=1, dtype='int16', samplerate=sample_rate)
+    stream.start()
+    return stream
+
+def load_messages(filename):
+    if os.path.exists(f'{filename}.json'):
+        with open(f'{filename}.json', 'r') as f:
+            return json.load(f)
+    else:
+        return [
+            {
+                "role": "system",
+                "content": "You are a note-taking program. Only respond in Markdown. The user is using a speech-to-text transcription. Do not communicate like a person. Notes can be added at the bottom if information is needed."
+            }
+        ]
+
+def save_messages(filename, messages):
+    with open(f'{filename}.json', 'w') as f:
+        json.dump(messages, f, indent=4)
+
+def transcribe_audio(buffer, whisper):
+    arr = np.array(buffer, dtype=np.int16).astype(np.float32) / 32768.0
+    return whisper.transcribe(arr)
+
+def get_chat_gpt_response(messages):
+    api_messages = [messages[0], messages[-2], messages[-1]] if len(messages) > 2 else messages
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=api_messages,
+        temperature=0,
+    )
+    return response["choices"][0]["message"]["content"]
+
+def write_markdown(filename, content):
+    with open(f'{filename}.md', 'w') as f:
+        f.write(content)
+
+def run_recording_session():
+    device_id = choose_device()
+    whisper = Whisper.from_pretrained("base.en")
+    filename = input("Enter a filename to save to (without extension): ")
+    messages = load_messages(filename)
+
+    buffer = []
+    recording = False
+
+    def callback(indata, frames, time, status):
+        nonlocal buffer
+        if status:
+            print(status)
+        if recording:
+            buffer.extend(indata[:, 0])
+
+    stream = create_stream(callback, device_id)
+
+    print('Press Enter to start/stop recording, q to quit')
+    while True:
+        command = input()
+        if command == '':
+            if recording:
+                recording = False
+                print('Stopped recording')
+
+                transcription = transcribe_audio(buffer, whisper)
+                print(f'Transcription: {transcription}')
+                messages.append({"role": "user", "content": transcription})
+
+                response_content = get_chat_gpt_response(messages)
+                messages.append({"role": "assistant", "content": response_content})
+
+                write_markdown(filename, response_content)
+                save_messages(filename, messages)
+
+                buffer = []
+            else:
+                print('Started recording')
+                recording = True
+        elif command.lower() == 'q':
             break
         else:
-            print(f"Device ID {device_id} is not an input device. Please try again.")
-    except (IndexError, ValueError):
-        print("Invalid device ID. Please try again.")
+            print('Invalid command')
+    stream.stop()
+    stream.close()
 
-# Set device
-sd.default.device = device_id
-
-# Define sample rate and duration (to be overwritten later)
-sample_rate = 16000  # This could change based on your device
-duration = 10  # Temporary duration
-
-# Initialize buffer and recording flag
-buffer = []
-recording = False
-
-# Define callback for the stream
-def callback(indata, frames, time, status):
-    global buffer
-    if status:
-        print(status)
-    if recording:
-        buffer.extend(indata[:, 0])
-
-# Create stream
-stream = sd.InputStream(callback=callback, channels=1, dtype='int16', samplerate=sample_rate)
-stream.start()
-
-# Initialize whisper
-w = Whisper.from_pretrained("base.en")
-
-# Prompt user for filename
-filename = input("Enter a filename to save to (without extension): ")
-    
-if os.path.exists(f'{filename}.json'):
-    # Load messages from .json file
-    with open(f'{filename}.json', 'r') as f:
-        messages = json.load(f)
-else:
-    # Initialize messages array
-    messages = [
-        {
-            "role": "system",
-            "content": "You are a note-taking program. Only respond in Markdown. The user is using a speech-to-text transcription. Do not communicate like a person. Notes can be added at the bottom if information is needed."
-        }
-    ]
-
-print('Press Enter to start/stop recording, q to quit')
-while True:
-    command = input()
-    if command == '':
-        if recording:
-            # Stop recording
-            recording = False
-            print('Stopped recording')
-
-            # Convert buffer into numpy array
-            buffer = np.array(buffer, dtype=np.int16)
-            
-            # Transcribe the audio
-            arr = buffer.astype(np.float32) / 32768.0
-            transcription = w.transcribe(arr)
-            print(f'Transcription: {transcription}')
-
-            # Add transcription to messages
-            messages.append({
-                "role": "user",
-                "content": transcription
-            })
-
-            # Construct a list of messages to send to the API
-            if len(messages) > 2:
-                api_messages = [messages[0], messages[-2], messages[-1]]
-            else:
-                api_messages = messages
-
-            # Get response from ChatGPT
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=api_messages,
-                temperature=0,
-            )
-            response_content = response["choices"][0]["message"]["content"]
-
-            # Append response to messages
-            messages.append({
-                "role": "assistant",
-                "content": response_content
-            })
-
-            # Write response content to .md file
-            with open(f'{filename}.md', 'w') as f:
-                f.write(response_content)
-
-            # Save messages array to .json file
-            with open(f'{filename}.json', 'w') as f:
-                json.dump(messages, f, indent=4)
-
-            # Clear the buffer
-            buffer = []
-        else:
-            # Start recording
-            print('Started recording')
-            recording = True
-    elif command.lower() == 'q':
-        # Quit
-        break
-    else:
-        print('Invalid command')
-stream.stop()
-stream.close()
+if __name__ == "__main__":
+    run_recording_session()
